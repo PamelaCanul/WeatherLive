3a5f41da52c96e514995ab36145827e49809daa7
Pamela Canul <59714317+PamelaCanul@users.noreply.github.com>
2024-05-25 14:13:15 -0600

    Setting up deployment to AWS

--- weatherlive/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import WeatherLive from "../components/WeatherLive";
-
-export default function Home() {
-  return (
-    <div className="min-h-screen h-screen flex items-center justify-center overflow-auto">
-      <WeatherLive />
-    </div>
-  );
-}
